000ce03194876d0c967561b3f818eb763cf68490
isabel-viviana <isabel-viviana>
2026-09-14 11:32:24 -0500

    Rutas agregadas en api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AreaController; 
+use App\Http\Controllers\AreaController;
+use App\Http\Controllers\ApprenticeController;
+use App\Http\Controllers\ComputerController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\TrainingCenterController;
 
 Route::get('/areas', [AreaController::class, 'index']);
 Route::post('/areas', [AreaController::class, 'store']);
+
+
+Route::get('/apprentices', [ApprenticeController::class, 'index']);
+Route::post('/apprentices', [ApprenticeController::class, 'store']);
+
+
+Route::get('/computers', [ComputerController::class, 'index']);
+Route::post('/computers', [ComputerController::class, 'store']);
+
+
+Route::get('/courses', [CourseController::class, 'index']);
+Route::post('/courses', [CourseController::class, 'store']);
+
+
+Route::get('/teachers', [TeacherController::class, 'index']);
+Route::post('/teachers', [TeacherController::class, 'store']);
+
+
+Route::get('/training-centers', [TrainingCenterController::class, 'index']);
+Route::post('/training-centers', [TrainingCenterController::class, 'store']);
